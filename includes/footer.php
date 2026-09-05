42552dfaec7d2d5aff14c8d2e82bdf1e9300dd03
luya8281-tech <luya8281@gmail.com>
2026-09-05 11:16:24 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+            </div>
+        </main>
+    </div>
+    
+    <script src="../assets/js/main.js"></script>
+    <?php if(isset($extra_js)): ?>
+        <?= $extra_js ?>
+    <?php endif; ?>
+</body>
+</html>
